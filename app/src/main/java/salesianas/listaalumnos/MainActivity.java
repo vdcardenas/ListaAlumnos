825fc043060ded76c13bd2942172c2e6ec6de0fd
esaup <esausalesianas@gmail.com>
2016-12-01 18:35:19 +0100

    pusheo prueba

--- app/src/main/java/salesianas/listaalumnos/MainActivity.java
+++ app/src/main/java/salesianas/listaalumnos/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Esto lo he hecho yo por que soy mejor que tu", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
